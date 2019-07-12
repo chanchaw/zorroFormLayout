a222a93a424bd043ef3b888d606c744da53cdbe0
chanchaw <409223171@qq.com>
2019-07-12 12:37:42 +0800

    补充 mainForm 属性

--- src/app/pages/formlayout/formlayout.component.ts
+++ src/app/pages/formlayout/formlayout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-formlayout',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormlayoutComponent implements OnInit {
 
-  constructor() { }
+  mainForm:FormGroup;
+  constructor(
+    private fb:FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.mainForm = this.fb.group({
+      codeid:[null],billdate:[null],deliverytime:[null],clientid:[null]
+    });
   }
 
 }
